4990bbf72e6688a7b53398da3e474aeb45545468
Marwane Qada <marwaneqada@gmail.com>
2026-05-07 02:02:27 +0100

    test: isolate test database from docker runtime

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,29 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function createApplication()
+    {
+        $this->forceSafeTestingEnvironment();
+
+        return parent::createApplication();
+    }
+
+    private function forceSafeTestingEnvironment(): void
+    {
+        $variables = [
+            'APP_ENV' => 'testing',
+            'CACHE_STORE' => 'array',
+            'DB_CONNECTION' => 'sqlite',
+            'DB_DATABASE' => ':memory:',
+            'DB_URL' => '',
+            'QUEUE_CONNECTION' => 'sync',
+            'SESSION_DRIVER' => 'array',
+        ];
+
+        foreach ($variables as $key => $value) {
+            putenv("{$key}={$value}");
+            $_ENV[$key] = $value;
+            $_SERVER[$key] = $value;
+        }
+    }
 }
